8a8b84111c294300b36ab58f67038475b35fa6b8
douik123 <ahmed@predict-a.com>
2022-08-12 02:58:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 function App() {
+  const [msg, setMsg] = useState("Click to say Hello !");
+  const [inp, setInp] = useState("Message");
+  const [tab, setTab] = useState(["ahmed","mariem","ksia","houcem"]);
+  const [input, setInput] = useState("");
+  const addNewTab=(e)=>{
+    setTab([...tab,input]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1>Hello !</h1>
+      <div className="counter-container">
+        <h3>{msg}</h3>
+        
+        <button 
+          onClick={() => {
+            setMsg("Hello !");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Change
+        </button>
+      </div>
+      <label>Text</label>
+      <input type="text" 
+      onChange={(e)=>setInp(e.target.value)}
+      />
+      <h1>{inp}</h1>
+      <div>
+      <input type="text" onChange={(e)=>setInput(e.target.value)}/><button onClick={addNewTab} >Save</button>
+      <div>
+       {tab.map(tabs=>
+        <div className="tabs">
+          <h3>{tabs}</h3>
+          <button>Delete</button>
+        </div>
+          )}
+      </div>
+
+
+
+
+      </div>
     </div>
   );
 }
-
 export default App;
